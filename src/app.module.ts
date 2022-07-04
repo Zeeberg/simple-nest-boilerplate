562cfd649493ce710f7ddc1c4e39337bcd127780
Zeeberg <danik18rus@gmail.com>
2022-07-04 17:37:15 +0400

    add: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ApiConfigService } from './shared/services/api-config.service';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
@@ -8,6 +10,12 @@ import { SharedModule } from './shared/shared.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [SharedModule],
+      useFactory: (configService: ApiConfigService) =>
+        configService.postgresConfig,
+      inject: [ApiConfigService],
     }),
     SharedModule,
   ],
--- src/shared/services/api-config.service.ts
+++ src/shared/services/api-config.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { isNil } from 'lodash';
+
+import { SnakeNamingStrategy } from '../../snake-naming.strategy';
 
 @Injectable()
 // eslint-disable-next-line @moneteam/nestjs/injectable-should-be-provided
@@ -47,6 +50,31 @@ export class ApiConfigService {
     const value = this.get(key);
 
     return value.replace(/\\n/g, '\n');
+  }
+
+  get postgresConfig(): TypeOrmModuleOptions {
+    const entities = [
+      __dirname + '/../../modules/**/*.entity{.ts,.js}',
+      __dirname + '/../../modules/**/*.view-entity{.ts,.js}',
+    ];
+    const migrations = [__dirname + '/../../database/migrations/*{.ts,.js}'];
+
+    return {
+      entities,
+      migrations,
+      keepConnectionAlive: !this.isTest,
+      dropSchema: this.isTest,
+      type: 'postgres',
+      name: 'default',
+      host: this.getString('DB_HOST'),
+      port: this.getNumber('DB_PORT'),
+      username: this.getString('DB_USERNAME'),
+      password: this.getString('DB_PASSWORD'),
+      database: this.getString('DB_DATABASE'),
+      migrationsRun: true,
+      logging: this.getBoolean('ENABLE_ORM_LOGS'),
+      namingStrategy: new SnakeNamingStrategy(),
+    };
   }
 
   get nodeEnv(): string {
